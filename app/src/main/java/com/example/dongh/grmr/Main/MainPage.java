f7ba3dcfa6fc464224ff79b7c799b4188ff393d4
jgunha <jgunha@gmail.com>
2019-01-08 19:36:07 +0900

    메인페이지, 인기 급상승 여행지, 하단바 기본 뼈대만 update

---
+++ app/src/main/java/com/example/dongh/grmr/Main/MainPage.java
@@ -0,0 +1,15 @@
+package com.example.dongh.grmr.Main;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.dongh.grmr.R;
+
+public class MainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_page);
+    }
+}
---
+++ app/src/main/java/com/example/dongh/grmr/Travel/PopPlace.java
@@ -0,0 +1,15 @@
+package com.example.dongh.grmr.Travel;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.dongh.grmr.R;
+
+public class PopPlace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popplace);
+    }
+}
